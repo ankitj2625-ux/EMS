d01795930337edb700fd6986f305db2214cc14e5
ankit-jibhakate-aj <112079450+ankit-jibhakate-aj@users.noreply.github.com>
2025-11-02 22:03:28 +0530

    added constants

--- src/main/java/com/hsbc/banking/EMS/constants/AppConstant.java
+++ src/main/java/com/hsbc/banking/EMS/constants/AppConstant.java
@@ -7,4 +7,10 @@ public class AppConstant {
     public static final String EMPLOYEE_DELETED_SUCCESS = "Employee deleted successfully";
     public static final String EMPLOYEE_ALREADY_EXISTS = "Employee already exists with email: ";
     public static final String INVALID_EMPLOYEE_DATA = "Invalid employee data provided";
+
+    public static final String DEFAULT_DEPARTMENT = "General";
+    public static final String HR_DEPARTMENT = "Human Resources";
+    public static final String IT_DEPARTMENT = "Information Technology";
+    public static final String FINANCE_DEPARTMENT = "Finance";
+
 }
